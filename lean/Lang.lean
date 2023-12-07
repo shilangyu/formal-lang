dd0e8ce8f3882a003f100ec54fb35156df145600
shilangyu <xmarcinmarcin@gmail.com>
2023-12-07 08:03:55 +0100

    Add lean setup

---
+++ lean/Lang.lean
@@ -0,0 +1,4 @@
+-- This module serves as the root of the `Lang` library.
+-- Import modules here that should be built as part of the library.
+import «Lang».Ast
+import «Lang».Basic
---
+++ lean/Main.lean
@@ -0,0 +1,5 @@
+import «Lang»
+
+def main : IO Unit :=
+  let a := Ident.mk "asd"
+  IO.println s!"Hello, {hello}!"
