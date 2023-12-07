2fcf9c08e6069eb6500c691200aeec18394697af
shilangyu <xmarcinmarcin@gmail.com>
2023-12-07 12:54:53 +0100

    Assign to name only

--- lang/src/main/scala/lang/AST.scala
+++ lang/src/main/scala/lang/AST.scala
@@ -1,24 +1,26 @@
 package lang
 
+opaque type Name = String
+
 enum Expr {
   case True
   case False
   case Nand(val left: Expr, val right: Expr)
 
-  case Ident(val name: String)
+  case Ident(val name: Name)
 
   case Ref(val of: Expr)
   case Deref(val of: Expr)
 
   // TODO: introduce structs
   // case Struct(val fields: List[StructField])
-  // case StructField(val name: String, val value: Expr)
-  // case Field(val of: Expr, val name: String)
+  // case StructField(val name: Name, val value: Expr)
+  // case Field(val of: Expr, val name: Name)
 }
 
 enum Stmt {
   case Decl(val name: String, val value: Expr)
-  case Assign(val to: Expr, val value: Expr)
+  case Assign(val to: Name, val value: Expr)
 
   case If(val condition: Expr, val body: Stmt)
   case While(val condition: Expr, val body: Stmt)
@@ -27,5 +29,5 @@ enum Stmt {
 
   case Swap(val left: Expr, val right: Expr)
 
-  case Bye(val ref: Expr.Ident)
+  case Bye(val ref: Name)
 }
