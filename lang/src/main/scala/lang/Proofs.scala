c390aff571639426509fb86014a40308f16b1d7b
Franco Sainas <franco@sainas.me>
2023-12-12 14:54:34 +0100

    closed stmt executes proof

--- lang/src/main/scala/lang/Proofs.scala
+++ lang/src/main/scala/lang/Proofs.scala
@@ -30,82 +30,90 @@ object Proofs {
       case Left(exceptions) => !exceptions.contains(LangException.UndeclaredVariable)
   )
 
-  /*
   def closedStmtEvaluates(stmt: Stmt, state: State): Unit = {
     val env = state._1
     require(Checker.isStmtClosed(stmt, env)._1)
     stmt match
       case Decl(name, value) =>
-        assert(Checker.isExprClosed(value, env))
         closedExprEvaluates(value, state)
       case Assign(to, value) =>
-        assert(state._1.contains(to))
-        assert(Checker.isExprClosed(value, env))
         closedExprEvaluates(value, state)
+        assert(state._1.contains(to))
       case If(cond, body) =>
         assert(Checker.isExprClosed(cond, env))
         closedExprEvaluates(cond, state)
-        closedStmtEvaluates(body, state)
-      //case While(cond, body) => ()state, ??
-      case Seq(s1, s2) =>
-        assert(Checker.isStmtClosed(s1, env)._1)
+      case Seq(s1, _) =>
         closedStmtEvaluates(s1, state)
-        //val menv = Checker.isStmtClosed(s1, env)._2
-        //assert(Checker.isStmtClosed(s2, menv)._1)
-        assert(Checker.isStmtClosed(s2, env)._1)
-        //closedStmtEvaluates(s2, (menv, state._2, state._3))
-        closedStmtEvaluates(s2, state)
   }.ensuring(
-    Interpreter.evalStmt(stmt, state) match
+    Interpreter.traceStmt1(Cmd(stmt, state)) match
       case Right(_) => true
       case Left(exceptions) => !exceptions.contains(LangException.UndeclaredVariable)
   )
 
+  /*
   def test(stmt: Stmt, state: State): Unit = {
     require(Interpreter.evalStmt(stmt, state).isRight)
     val newState = Interpreter.evalStmt(stmt, state)
   }.ensuring(Interpreter)
-
-  def noLoc(stmt: Stmt, state: State): Unit = {
-    require(Interpreter.evalStmt(stmt, state).isRight) 
-    require(!state._2.contains(state._3))
-    val freeLoc = state._3
-    stmt match
-      case Decl(name, value)  => 
-        val Right(newState) = Interpreter.evalStmt(stmt, state)
-        //assert(newState.isRight)
-        assert(newState._3 == freeLoc+1)
-        assert(newState._2.contains(freeLoc))
-      case Assign(_,_)        => ()
-      case If(_, body)        =>
-        noLoc(body, state)
-      case Seq(s1, s2)        => 
-        noLoc(s1, state)
-        val Right(newState) = Interpreter.evalStmt(stmt, state)
-        noLoc(s2, newState)
-  }
   */
 
   def locIncreases(stmt: Stmt, state: State): Unit = {
-    require(Interpreter.traceStmt1(Cmd(stmt, state)).isRight)
-    Interpreter.traceStmt1(Cmd(stmt, state)) match
-      case Left(_)  => ()
-      case Right(St(newState)) => stmt match
-        case Decl(_, _) => 
-          assert(newState._3 == state._3 + 1)
-        case _ => ()
-      case Right(Cmd(_, newState)) => stmt match
-        case Decl(_, _) => 
-          assert(newState._3 == state._3 + 1)
-        case Seq(s, _) => s match
-          case Decl(_, _) =>
-            assert(newState._3 == state._3 + 1)
-          case _ => ()
-        case _ => ()
+    decreases(stmt)
+
+    stmt match
+      case Decl(_, _)   =>
+        Interpreter.traceStmt1(Cmd(stmt, state)) match
+          case Left(_)      => ()
+          case Right(conf)  => conf match
+            case St(newState) =>
+              assert(newState._3 == state._3 + 1)
+            case _ => ()
+      case Seq(s1, _)   => 
+        locIncreases(s1, state)
+      case _            => ()
+
   }.ensuring(
     Interpreter.traceStmt1(Cmd(stmt, state)) match
-      case Left(_)                  => true
-      case Right(St(newState))      => newState._3 >= state._3
-      case Right(Cmd(_, newState))  => newState._3 >= state._3
-      )
+      case Left(_) => true
+      case Right(conf) => conf match
+        case St(newState) => newState._3 >= state._3
+/*
+          if stmt.isInstanceOf[Decl] then newState._3 == state._3 + 1
+          else newState._3 == state._3
+          */
+        case Cmd(_, newState) => newState._3 >= state._3
+    )
+
+  /*
+  def noDoubleLoc(stmt: Stmt, state: State): Unit = {
+    //require(Interpreter.traceStmt1(stmt, state).isRight) 
+    decreases(stmt)
+    val mem = state._2
+    val freeLoc = state._3
+    require(!mem.contains(freeLoc))
+
+    locIncreases(stmt, state)
+    stmt match
+      case Decl(name, value)  =>
+        Interpreter.traceStmt1(Cmd(stmt, state)) match
+        //assert(newState._3 == freeLoc+1)
+          case Left(_)      => ()
+          case Right(conf)  => conf match
+            case St(newState) => 
+              assert(newState._2.contains(state._3))
+            case Cmd(_, newState) => ()
+      case Seq(s1, _)        => 
+        noDoubleLoc(s1, state)
+      case _ => ()
+  }.ensuring(
+    Interpreter.traceStmt1(Cmd(stmt, state)) match
+      case Left(_) => true
+      case Right(conf) => conf match
+        case St(newState) => 
+          newState._2 == state._2 || newState._2 - state._3 == state._2
+        case Cmd(_, newState) => 
+          newState._2 == state._2 || newState._2 - state._3 == state._2
+    )
+  */
+
 }
